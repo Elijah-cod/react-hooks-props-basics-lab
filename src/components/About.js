b123a3a5702f64d987e8f2179142f85071318a20
Elijah <iamngunjiri@gmail.com>
2025-01-15 12:10:14 +0300

    Complete Props Basics Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,13 @@
 import React from "react";
-
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+import WithBio from "./WithBio";
+import WithoutBio from "./WithoutBio";
+function About(props) {
+  if (props.bio) {
+    return <WithBio bio = {props.bio} github={props.github} linkedin={props.linkedin}/>
+  } else {
+    return <WithoutBio github={props.github} linkedin={props.linkedin}/>
+  }
 }
+
 
 export default About;
---
+++ src/components/WithoutBio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Links from "./Links";
+
+function WithoutBio(props) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.github} linkedin={props.linkedin} />
+    </div>
+  );
+}
+
+export default WithoutBio;
